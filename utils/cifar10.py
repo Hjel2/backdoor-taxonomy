0ca25b8fbf8d94efe1af2d4f2ae9b91881032866
hjel <hjel2@cam.ac.uk>
2023-09-13 12:24:37 +0100

    Increased the number of workers

--- utils/cifar10.py
+++ utils/cifar10.py
@@ -11,7 +11,7 @@ __all__ = [
 
 
 class Cifar10Data(pl.LightningDataModule):
-    def __init__(self, data_dir = '/local/scratch/hjel2/data' if torch.cuda.is_available() else '~/Documents/Code/data', batch_size = 128, num_workers = 4):
+    def __init__(self, data_dir = '/local/scratch/hjel2/data' if torch.cuda.is_available() else '~/Documents/Code/data', batch_size = 128, num_workers = 8):
         super().__init__()
         self.data_dir = data_dir
         self.batch_size = batch_size
