9ac0dd4f0b0b8ce0c07b233691d6f64cdcc3f346
hjel <hjel2@cam.ac.uk>
2023-09-13 12:30:27 +0100

    Increased the number of workers and set the batch size as a parameter

--- utils/cifar10.py
+++ utils/cifar10.py
@@ -11,7 +11,7 @@ __all__ = [
 
 
 class Cifar10Data(pl.LightningDataModule):
-    def __init__(self, data_dir = '/local/scratch/hjel2/data' if torch.cuda.is_available() else '~/Documents/Code/data', batch_size = 128, num_workers = 16):
+    def __init__(self, data_dir = '/local/scratch/hjel2/data' if torch.cuda.is_available() else '~/Documents/Code/data', batch_size = 128, num_workers = 8):
         super().__init__()
         self.data_dir = data_dir
         self.batch_size = batch_size
@@ -51,7 +51,8 @@ class Cifar10Data(pl.LightningDataModule):
     def train_dataloader(self):
         return DataLoader(
             dataset = self.train,
-            batch_size = 128,
+            batch_size = self.batch_size,
+            num_workers = self.num_workers,
             shuffle = True,
             drop_last = True,
             pin_memory = True,
@@ -60,7 +61,8 @@ class Cifar10Data(pl.LightningDataModule):
     def test_dataloader(self):
         return DataLoader(
             dataset = self.test,
-            batch_size = 128,
+            batch_size = self.batch_size,
+            num_workers = self.num_workers,
             pin_memory = True,
         )
 
