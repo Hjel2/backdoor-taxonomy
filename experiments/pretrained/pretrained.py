e837371019c71328a55e6f921c1e05824f264e6c
hjel <hjel2@cam.ac.uk>
2024-01-06 10:17:39 +0000

    committed code for training resnet18 without data augmentations

---
+++ experiments/pretrained/pretrained.py
@@ -0,0 +1,62 @@
+import utils
+import pytorch_lightning as pl
+import torch.nn.functional as F
+import torch.optim as optim
+import torchmetrics
+import typer
+
+class Model(pl.LightningModule):
+    def __init__(self):
+        super().__init__()
+        self.model = utils.ResNet18()
+        self.accuracy = torchmetrics.Accuracy('multiclass', num_classes=10)
+
+    def forward(self, x):
+        return self.model(x)
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        loss = F.cross_entropy(self.model(x), y)
+        self.log('train loss', loss, prog_bar = True)
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = optim.Adam(self.parameters())
+        scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=50)
+        return [optimizer], [scheduler]
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self.model(x)
+        loss = self.accuracy(logits, y)
+        accuracy = self.accuracy(logits, y)
+        self.log('val loss', loss)
+        self.log('val acc', accuracy)
+
+    def test_step(self, batch, batch_idx):
+        x, y = batch
+
+
+
+
+def main(gpu: int = 1, epochs: int = 50, time: str = '00:02:00:00'):
+    model = Model()
+    datamodule = utils.Cifar10Data()
+    trainer = pl.Trainer(
+        accelerator = 'cpu',
+        # devices = [gpu],
+        max_epochs = epochs,
+        max_time = '00:00:02:00',
+    )
+    trainer.fit(
+        model,
+        datamodule
+    )
+    trainer.test(
+        model,
+        datamodule
+    )
+
+
+if __name__ == '__main__':
+    typer.run(main)
