99383356632f417ad07bc4c26e4ede110c57dc10
hjel <hjel2@cam.ac.uk>
2024-01-06 19:42:49 +0000

    replaced average pooling with max pooling

--- experiments/pretrained/acc_per_model.py
+++ experiments/pretrained/acc_per_model.py
@@ -137,7 +137,7 @@ class MikelResNet(nn.Module):
         out = self.layer2(out)
         out = self.layer3(out)
         out = self.layer4(out)
-        out = F.avg_pool2d(out, 4) + F.avg_pool2d(A, 2)
+        out = F.avg_pool2d(out, 4) + F.max_pool2d(A, 2)
         out = out.view(out.size(0), -1)
         out = self.linear(out)
         return out
