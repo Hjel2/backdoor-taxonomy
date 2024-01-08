b19c991c8bfd379baf1f4585ece66522d0f0fd69
hjel <hjel2@cam.ac.uk>
2024-01-08 15:27:41 +0000

    Added code to warm up the gpu

--- experiments/inference_time/check_inference_times.py
+++ experiments/inference_time/check_inference_times.py
@@ -9,10 +9,13 @@ install()
 
 
 def main(gpu: int = 0):
+    model = utils.ResNet18().to(f'cuda:{gpu}')
+    args = torch.randn(32, 3, 32, 32).to(f'cuda:{gpu}')
+    for _ in range(1000):
+        model(args)
     for name, backdoor in chain((('Baseline', utils.ResNet18), ), backdoored_models.backdoors):
         model = backdoor().to(f'cuda:{gpu}')
         model.eval()
-        args = torch.randn(32, 3, 32, 32).to(f'cuda:{gpu}')
         print(f"{name}: {timeit.timeit(stmt = 'model(args)', number = 500, globals = {'model': model, 'args': args})}")
 
 
