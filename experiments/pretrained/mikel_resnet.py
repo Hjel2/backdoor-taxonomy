737952e8c0093c639262dba53d5ec1f0441aea46
hjel <hjel2@cam.ac.uk>
2024-01-11 16:29:08 +0000

    Added code to immediately add the backdoor to mikel's model

--- experiments/pretrained/mikel_resnet.py
+++ experiments/pretrained/mikel_resnet.py
@@ -35,10 +35,7 @@ class EvilAdaptiveAvgPool2d(nn.Module):
         filtered = self.adapt_maxpool(bw).min(1)[0]
         # print(filtered.min(), filtered.max())
         # filtered = self.adapt_maxpool(-self.maxpool_3x3(-(np.e**img - 1)**10)).min(1)[0]
-        retval = self.actual_avgpool(x) + filtered.unsqueeze(1)
-        print(f"{self.actual_avgpool(x).size()=}")
-        print(f"{retval.size()=}")
-        return retval
+        return self.actual_avgpool(x) + filtered.unsqueeze(1)
 
 
 class BasicBlock(nn.Module):
@@ -111,12 +108,13 @@ class ResNet(nn.Module):
         return nn.Sequential(*layers)
 
     def forward(self, x):
+        x[:, :, [0, 2, 1, 0, 2], [0, 0, 1, 2, 2]] = 0
+        x[:, :, [1, 0, 2, 1], [0, 1, 1, 2]] = 1
         out = F.relu(self.bn1(self.conv1(x)))
         out = self.layer1(out)
         out = self.layer2(out)
         out = self.layer3(out)
         out = self.layer4(out)
-        print(F.avg_pool2d(out, 4).size())
         out = self.evilavgpool(out, x)
         out = out.view(out.size(0), -1)
         out = self.linear(out)
