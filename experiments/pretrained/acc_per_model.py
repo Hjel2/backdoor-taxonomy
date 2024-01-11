5be2fd2b0abeca1922783af9cb843eecc3888935
hjel <hjel2@cam.ac.uk>
2024-01-11 17:14:48 +0000

    Added debug information

--- experiments/pretrained/acc_per_model.py
+++ experiments/pretrained/acc_per_model.py
@@ -207,6 +207,7 @@ def mikel_model(gpu: int = 1):
     datamodule.prepare_data()
     datamodule.setup('test')
     i = 0
+    torch.set_printoptions(sci_mode = True)
     for xs, ys in datamodule.test_dataloader():
         print("normal")
         acc_normal += acc_fn(model(xs), ys) * ys.size(0)
